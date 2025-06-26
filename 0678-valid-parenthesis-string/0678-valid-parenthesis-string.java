82f85b3c91075ffaa1bd908298f0748e9b909587
VIGNESH K <107059364+vigneshvicky2@users.noreply.github.com>
2025-06-26 12:30:31 +0530

    Time: 1 ms (34.4%), Space: 41.6 MB (44.22%) - LeetHub

---
+++ 0678-valid-parenthesis-string/0678-valid-parenthesis-string.java
@@ -0,0 +1,23 @@
+class Solution {
+    public boolean checkValidString(String s) {
+        Stack<Integer> br = new Stack<>();
+        Stack<Integer> sr = new Stack<>();
+        for(int i =0; i<s.length();i++){
+            char ch = s.charAt(i);
+            if(ch=='(') br.push(i);
+            else if(ch=='*') sr.push(i);
+            else if( ch==')'){
+                if(!br.isEmpty()) br.pop();
+                else if(!sr.isEmpty()) sr.pop();
+                else return false;
+            }
+        }
+        while(!sr.isEmpty() && !br.isEmpty()){
+            int open_ind = br.pop();
+            int close_ind = sr.pop();
+            if(open_ind>close_ind) return false;
+        }
+        return br.isEmpty();
+    }
+}
+// (((((()*)(*)*))())())(()())())))((**)))))(()())()
